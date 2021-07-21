09f2249eef04514cfbd51e8ead446eae1bba69a8
Kazuhiro Abe <shurrasco@yahoo.co.jp>
2021-07-21 18:11:04 +0900

    first commit

---
+++ XCtestSample/XCtestSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCtestSampleApp.swift
+//  XCtestSample
+//
+//  Created by Kazuhiro Abe on 2021/07/21.
+//
+
+import SwiftUI
+
+@main
+struct XCtestSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
